fa39c6b0d4ed5262da21d2d7bcd60df7b5f73638
willzfarmer <willzfarmer@gmail.com>
2013-04-25 10:38:40 -0600

    AI follows ships

--- bin/battleshipAI.py
+++ bin/battleshipAI.py
@@ -11,12 +11,13 @@ class BattleshipAI:
         '''
         Artificial Intelligence Player for Battleship
         '''
-        self.name     = player.name
-        self.gridsize = player.gridsize
-        self.guesses  = player.guesses
-        self.grid     = player.grid
-        self.player   = player
-        self.level    = random.randint(0, 2)
+        self.name      = player.name
+        self.gridsize  = player.gridsize
+        self.guesses   = player.guesses
+        self.grid      = player.grid
+        self.player    = player
+        self.level     = random.randint(0, 2)
+        self.last_shot = None
 
     def is_shot(self, x, y, gridsize, pid):
         '''
@@ -25,8 +26,13 @@ class BattleshipAI:
         for item in self.guesses:
             if item.pid == pid:
                 current_guesses = item.guesses
-        if current_guesses[y][x] != 0:
+        try:
+            current_guesses[y][x]
+            if current_guesses[y][x] != 0:
+                return True
+        except IndexError:
             return True
+        return False
 
     def get_rand_spot(self, gridsize, pid):
         '''
@@ -67,16 +73,44 @@ class BattleshipAI:
                     break
         return x, y
 
+    def follow(self, gridsize, pid):
+        '''
+        Follows a coordinate
+        '''
+        x = self.last_shot[0]
+        y = self.last_shot[1]
+        conflict = self.is_shot(x, y, gridsize, pid)
+        num_list = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while conflict:
+            for number in range(len(num_list)):
+                if num_list[number] is not None:
+                    x += num_list[number][0]
+                    y += num_list[number][1]
+                    num_list[number] = None
+                    conflict = self.is_shot(x, y, gridsize, pid)
+                    if conflict:
+                        x = self.last_shot[0]
+                        y = self.last_shot[1]
+                    else:
+                        break
+                elif num_list == [None, None, None, None]:
+                    x, y = self.get_rand_spot(gridsize, pid)
+                    conflict = False
+        return x, y
+
     def get_position(self, playerlist):
         '''
-        Shoots at a random player
+        Shoots at a random player in a designated pattern
         '''
         pid = random.randint(0, len(playerlist) - 1)
         while playerlist[pid].state != True:
             pid = random.randint(0, len(playerlist) - 1)
         while pid == self.name:
             pid = random.randint(0, len(playerlist) - 1)
-        x, y = self.diagonal(self.gridsize, pid)
+        if self.last_shot:
+            x, y = self.follow(self.gridsize, pid)
+        else:
+            x, y = self.diagonal(self.gridsize, pid)
         if x is False or y is False:
             x, y = self.get_rand_spot(self.gridsize, pid)
         return pid, x, y
@@ -94,12 +128,12 @@ class BattleshipAI:
             result = item.register(x, y)
             if result == True:
                 current_guesses[y][x] = 1
-#                self.player.guesses[y][x] = 1
+                self.last_shot = (x, y)
                 break
             elif result == False:
                 current_guesses[y][x] = -1
-#                self.player.guesses[y][x] = -1
+                self.last_shot = None
             else:
                 current_guesses[y][x] = 1
-#                self.player.guesses[y][x] = 1
+                self.last_shot = (x, y)
                 break
